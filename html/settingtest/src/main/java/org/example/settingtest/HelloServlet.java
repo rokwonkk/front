9bfe38286f01665700221ebc7febb3320dac1066
rokwonkk <jirisan8147@gmail.com>
2023-12-19 17:44:30 +0900

    아이프레임, 목록, 테이블 작성 및 적용

---
+++ html/settingtest/src/main/java/org/example/settingtest/HelloServlet.java
@@ -0,0 +1,28 @@
+package org.example.settingtest;
+
+import java.io.*;
+
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+@WebServlet(name = "helloServlet", value = "/hello-servlet")
+public class HelloServlet extends HttpServlet {
+    private String message;
+
+    public void init() {
+        message = "Hello World! 하이용";
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+
+        // Hello
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h1>" + message + "</h1>");
+        out.println("</body></html>");
+    }
+
+    public void destroy() {
+    }
+}
